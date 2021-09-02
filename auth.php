049aa6fc942974b1487684f5824e4a7702e9eacf
fclement-ngmedia <52401789+fclement-ngmedia@users.noreply.github.com>
2021-09-02 22:52:48 +0200

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,19 +1,20 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 
-$ngmsUsername = "NGMediaServerAdministratorUsername";
-$ngmsPassword = "NGMediaServerAdministratorPassword";
-$callTo = $_GET["to"];
+// The following settings must match those of your NG Media Server
+$ngmsUsername = 'your-ngms-administrator-username';
+$ngmsPassword = 'your-ngms-administrator-password';
 
-function hmacSha1( $ngmsPassword ,  $data ){
-	$hmac = hash_hmac('sha1', $data, $ngmsPassword);
-	return base64_encode(pack('H*',$hmac)); 
+function hmacSha1($data, $ngmsPassword){
+  $hmac = hash_hmac('sha1', $data, $ngmsPassword);
+  return base64_encode(pack('H*', $hmac)); 
 }
 
-$data = "\n\n".$callTo."\n\n\n\n\n\n";
-$expiry = (floor(microtime(true)) + 10);
+$data = '\n\n' . $_GET["to"] . '\n\n' . $_GET["from"] . '\n\n\n\n';
+$validityPeriod = 10;  // In seconds
+$expiry = (floor(microtime(true)) + $validityPeriod); // Number of seconds since January 1, 1970
 $tmpUsername = $expiry . ':' . $ngmsUsername;
 
-$authorization = hmacSha1($ngmsPassword ,  $data  .  $tmpUsername )  .  ':'  .  $tmpUsername ;
-echo $authorization;
+$authorization = hmacSha1($data . $tmpUsername, $ngmsPassword) . ':' . $tmpUsername;
+echo '{ authorization: "' . $authorization . '" }';
 ?>
