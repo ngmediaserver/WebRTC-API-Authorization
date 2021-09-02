c38f0918473bcca0f1b259a0b9fe099098c32d9b
fclement-ngmedia <52401789+fclement-ngmedia@users.noreply.github.com>
2021-09-02 23:01:19 +0200

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,15 +1,23 @@
 <?php
+// Sample Code - PHP
 header('Access-Control-Allow-Origin: *');
 
 // The following settings must match those of your NG Media Server
 $ngmsUsername = 'your-ngms-administrator-username';
 $ngmsPassword = 'your-ngms-administrator-password';
 
+// This function generates the authorization token
 function hmacSha1($data, $ngmsPassword){
   $hmac = hash_hmac('sha1', $data, $ngmsPassword);
   return base64_encode(pack('H*', $hmac)); 
 }
 
+// With this sample, the client page must performs a GET on auth.php in order to get the authorization parameter
+// input (query string):
+// - to: the requested called URI (/number)
+// - from (optional): an optional requested caller URI (/number)
+// output (JSON):
+// - authorization: the authorization parameter to pass in MakeCall or Register.
 $data = '\n\n' . $_GET["to"] . '\n\n' . $_GET["from"] . '\n\n\n\n';
 $validityPeriod = 10;  // In seconds
 $expiry = (floor(microtime(true)) + $validityPeriod); // Number of seconds since January 1, 1970
