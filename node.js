49c6b2cd569cda1b56ee611f4312e483060bf106
fclement-ngmedia <52401789+fclement-ngmedia@users.noreply.github.com>
2021-09-02 23:35:35 +0200

    Update node.js

--- node.js
+++ node.js
@@ -43,17 +43,12 @@ async function main() {
     });
   }
 
-  server.route({
-    // With this sample, the client page must performs a GET on /api/computeAuthorization in order to get the authorization parameter
-    // input:
-    // - to: the requested called URI (/number)
-    // - from (optional): an optional requested caller URI (/number)
-    // output:
-    // - authorization: the authorization parameter to pass in MakeCall or Register.
-    // - from (optional): the Caller URI (/number) that should be used. 
-    //   A returned from parameter is needed only when this information is not already known by the client page.
-    //   For example, the user may have started a session with the identifier "johnsmith@localdomain.com", but when 
-    //   making a call, the associated Caller URI (/number) 3005 should be provided to NG Media Server for that user.
+  // With this sample, the client page must performs a GET on /api/computeAuthorization in order to get the authorization parameter
+  // input (query string):
+  // - to: the requested called URI (/number)
+  // output (JSON):
+  // - authorization: the authorization parameter to pass in MakeCall or Register.
+  // - from (optional): the from parameter that was used (if any) ; see below
     method: 'GET',
     path: '/api/computeAuthorization',
     handler: async (request, reply) => {
@@ -61,19 +56,19 @@ async function main() {
       // *** Here your code MUST verify that the client page requesting the authorization token is allowed. ***
       // *** Typically you MAY check that the requests is associated with an active user session. ***
       // *** An authorization parameter MUST be delivered only if all input parameters are allowed. ***
-      // The authorization parameter signs the input parameters (and the optional output parameter 'from')
+      // The authorization parameter signs the input parameters
 
-      if (request.query.to !== undefined) {
-        let data = '\n\n' + request.query.to + '\n\n' + request.query.from + '\n\n\n\n';
+      let paramTo       = request.query.to  ;
+//    let paramFrom     = request.query.from    ;
+      let paramFrom     = undefined; // Here, the from parameter is typically empty (anonymous) or set according to the user logged in this session
+      if (paramTo !== undefined) {
+        let data = '\n\n' + paramTo + '\n\n' + ((paramFrom === undefined) ? "" : paramFrom) + '\n\n\n\n';
         const validityPeriod = 10;  // In seconds
         let expiry = (Math.floor(new Date() / 1000) + validityPeriod); // Number of seconds since January 1, 1970
         let tmpUsername = expiry + ':' + ngmsUsername;
 
         const authorization = await hmacSha1(data + tmpUsername, ngmsPassword) + ':' + tmpUsername;
-        return {
-          authorization: authorization,
-//        from: request.query.from,
-        }
+        return { authorization: authorization, from: paramFrom, }
       }else{			
         const err = {
           cause: 1,
